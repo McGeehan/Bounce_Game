d3f5e07b1e9160ed46faf99af026295ff772f7f7
McGeehan Coding <mmcgeehan@paduaacademy.org>
2016-09-13 10:53:11 -0400

    Try this

--- Bouncey Bouncers/Assets/Scripts/CoinStuff.js
+++ Bouncey Bouncers/Assets/Scripts/CoinStuff.js
@@ -2,10 +2,8 @@
 
 public var points : int = 5; 
 public var pickedUpBy : String = "Player";  
-
- 
-					
-function OnTriggerEnter2D(other : Collider2D)
+														
+function OnTriggerEnter2D(other : Collider2D) //in this function our parameter is other and it is of type Collider2D
 {
 	if ( other.CompareTag(pickedUpBy) )
 	{
