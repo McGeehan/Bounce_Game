95d224a2c1d6b2fb7f5b0b3028557c2dacabba50
McGeehan Coding <mmcgeehan@paduaacademy.org>
2016-09-30 11:35:06 -0400

    yupppp

--- Bouncey Bouncers/Assets/Scripts/BombHit.js
+++ Bouncey Bouncers/Assets/Scripts/BombHit.js
@@ -2,12 +2,25 @@
 
 function OnCollisionEnter2D (other : Collision2D) //When collisions occur, the bomb dissappears
 {
+
 	if (other.gameObject.CompareTag("Ground"))
 	{
 		GameData.numMissed++; //also show shortcut NumMissed++;
 		GameData.DisplayScore();
 		Destroy(gameObject);
 
+
 	}
 }
 
+public var radius: float = 5.0F;
+public var power: float = 10.0F;
+function Start() {
+	var explosionPos: Vector3 = transform.position;
+	var colliders: Collider[] = Physics.OverlapSphere(explosionPos, radius);
+	for (var hit: Collider in colliders) {
+		var rb: Rigidbody = hit.GetComponent.<Rigidbody>();
+		if (rb != null)
+			rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+	}
+}
